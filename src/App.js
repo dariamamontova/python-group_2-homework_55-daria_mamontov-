2228b911c77cf2c56c932b73adb5cc4852de1058
Daria <mamontova.dasha@gmail.com>
2019-02-05 17:32:09 +0600

    Добавлены функции добавления и удаления ингредиента.

--- src/App.js
+++ src/App.js
@@ -20,12 +20,46 @@ class App extends Component {
         }
     };
 
+    addIngredient = (name) => {
+        let ingredient = {...this.state.ingredients[name]};
+        let price = availableIngredients.find(item => item.name === name).price;
+        ingredient.count += 1;
+        ingredient.total = ingredient.count * price;
+        let ingredients = {...this.state.ingredients};
+        ingredients[name] = ingredient;
+        let state = {...this.state};
+        state.ingredients = ingredients;
+        this.setState(state);
+    };
+
+    removeIngredient = (name) => {
+        let ingredient = {...this.state.ingredients[name]};
+        let price = availableIngredients.find(item => item.name === name).price;
+        if (ingredient.count > 0) {
+            ingredient.count -= 1;
+        }
+        ingredient.total = ingredient.count * price;
+
+        let ingredients = {...this.state.ingredients};
+        ingredients[name] = ingredient;
+
+        let state = {...this.state};
+        state.ingredients = ingredients;
+
+        this.setState(state);
+    };
 
 
     render() {
         return (
             <div className="App">
                 <Burger ingredients={this.state.ingredients}/>
+                {/* здесь вывести панель с общей ценой */}
+                {/* для подсчёта цены можно добавить метод в App.js */}
+                {/* и вызвать его в фигурных скобках в JSX, */}
+                {/* чтобы получить и вывести результат. */}
+                {/* под ценой вывести форму BurgerForm */}
+                {/* в форме вывести IngredientControl для каждого ингредиента */}
             </div>
         );
     }
