5d0e333808699139bf1bc87fa0cc8633eb469ebe
Daria <mamontova.dasha@gmail.com>
2019-02-07 15:54:45 +0600

    Добавлена функция подсчета общей суммы и вывод компонентов на странице.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
 import Burger from './components/Burger/Burger';
+import IngredientControl from './components/BurgerForm/IngredientControl';
+import BurgerForm from './components/BurgerForm/BurgerForm';
 
 const availableIngredients = [
     {name: 'salad', price: 5, label: 'Салат'},
@@ -13,11 +15,11 @@ const availableIngredients = [
 class App extends Component {
     state = {
         ingredients: {
-            salad: {count: 1, total: 0},
-            cheese: {count: 2, total: 0},
-            meat: {count: 2, total: 0},
-            bacon: {count: 1, total: 0}
-        }
+            salad: {count: 0, total: 0, label: 'Салат', price: 5},
+            cheese: {count: 0, total: 0, label: 'Сыр', price: 20},
+            meat: {count: 0, total: 0, label: 'Мясо', price: 30},
+            bacon: {count: 0, total: 0, label: 'Бекон', price: 20}
+        },
     };
 
     addIngredient = (name) => {
@@ -26,6 +28,7 @@ class App extends Component {
         ingredient.count += 1;
         ingredient.total = ingredient.count * price;
         let ingredients = {...this.state.ingredients};
+
         ingredients[name] = ingredient;
         let state = {...this.state};
         state.ingredients = ingredients;
@@ -49,20 +52,29 @@ class App extends Component {
         this.setState(state);
     };
 
+    getTotal = () => {
+        let ingredients = {...this.state.ingredients};
+        let sum = 0;
+        Object.keys(ingredients).map(item => {
+            sum += ingredients[item].total
+        })
+        return sum;
+    };
+
 
     render() {
         return (
             <div className="App">
                 <Burger ingredients={this.state.ingredients}/>
-                {/* здесь вывести панель с общей ценой */}
-                {/* для подсчёта цены можно добавить метод в App.js */}
-                {/* и вызвать его в фигурных скобках в JSX, */}
-                {/* чтобы получить и вывести результат. */}
-                {/* под ценой вывести форму BurgerForm */}
-                {/* в форме вывести IngredientControl для каждого ингредиента */}
+                <BurgerForm getTotal={this.getTotal}>
+                    {availableIngredients.map(item => <IngredientControl name={item.name}
+                     onAddIngredient={() => this.addIngredient(item.name)}
+                     onRemoveIngredient={() => this.removeIngredient(item.name)}/>)}
+                </BurgerForm>
             </div>
         );
     }
 }
 
 export default App;
+
